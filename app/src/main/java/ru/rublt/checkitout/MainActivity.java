f80bf3fc14bdcab2f05762f10e840c1a8091ce01
x038xx77 <x038xx77@gmail.com>
2020-05-13 19:15:31 +0300

    Checkitout start 2.0

---
+++ app/src/main/java/ru/rublt/checkitout/MainActivity.java
@@ -0,0 +1,22 @@
+package ru.rublt.checkitout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.onesignal.OneSignal;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
